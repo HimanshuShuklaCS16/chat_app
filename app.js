015f657bfd0871c1022d93bd059b502353f3120e
himanshu shukla <himanshushukla16398@gmail.com>
2019-08-07 16:40:45 +0530

    room no , namespace , event handelling concepts used

--- app.js
+++ app.js
@@ -6,12 +6,29 @@ var io = require('socket.io')(server);
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
+var clients = 0;
+var roomno = 1;
 io.on('connection', (socket) => {
+    //a welcome message to a new client and a no. of clients message to the remaining sockets
+    clients++;
+    socket.emit('newclientconnect',{description : "hey welcome to the chat !!"});
+    socket.broadcast.emit('newclientconnect',{description : clients + "clients connected"});
+
 console.log('a user is connected');
+//room num. setup
+if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1)roomno++;//max of 2 clients in a room
+socket.join("room-"+roomno);
+io.sockets.in("room-"+roomno).emit('connecttoroom',"you are in room no."+roomno);//room no. msg to the socket
+//chat message event handle
 socket.on('chat message',(msg) => {
     io.emit('chat message',msg);
     console.log(msg);
+});
+//user disconnected event
+socket.on('disconnect' , () => {
+    clients--;
+    io.sockets.emit('newclientconnect',{description : clients + "clients connected"});
+    socket.leave("room-"+roomno);
 });
 });
 
