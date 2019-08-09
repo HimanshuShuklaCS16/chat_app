10ed01c528d5670e370fe85bbff9e518707fe761
himanshu shukla <himanshushukla16398@gmail.com>
2019-08-09 16:39:01 +0530

    username configuration done

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ app.get('/',(req,res) => {
 });
 var clients = 0;
 var roomno = 1;
+var users = [];
 io.on('connection', (socket) => {
     //a welcome message to a new client and a no. of clients message to the remaining sockets
-    clients++;
-    socket.emit('newclientconnect',{description : "hey welcome to the chat !!"});
-    socket.broadcast.emit('newclientconnect',{description : clients + "clients connected"});
-
+//username verify event
+socket.on("setUsername",(data) => {
+    if(users.indexOf(data) == -1){
+        clients++;
+        socket.emit('newclientconnect',{description : "hey welcome to the chat !!"});
+        socket.broadcast.emit('newclientconnect',{description : clients + "clients connected"});
+        users.push(data);
+        socket.emit("userSet", {username : data});
+    }
+    else {
+        socket.emit("userExists",data + " username is already taken !! try some other username.");
+    }
+});
 console.log('a user is connected');
 //room num. setup
 if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1)roomno++;//max of 2 clients in a room
@@ -29,6 +39,10 @@ socket.on('disconnect' , () => {
     clients--;
     io.sockets.emit('newclientconnect',{description : clients + "clients connected"});
     socket.leave("room-"+roomno);
+});
+//error handlling
+socket.on('connect_failed',() => {
+    document.write("sorry , their seems to be an error in the connection");
 });
 });
 
